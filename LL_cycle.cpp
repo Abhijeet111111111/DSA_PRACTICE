6364dce929cb2501368356e532f84f8d0197d2a7
Aj <yabhijeet@gmai.com>
2025-07-13 16:31:38 +0530

    another sol

--- LL_cycle.cpp
+++ LL_cycle.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
-  struct ListNode
-  {
-      int val;
-      ListNode *next;
-      ListNode(int x) : val(x), next(NULL) {}
-  };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
-  class Solution
-  {
-  public:
-      bool hasCycle(ListNode *head)
-      {
-          int count = 0;
-          while (head)
-          {
-              if (count > 100002)
-                  return true;
-              head = head->next;
-              count++;
-          }
-          return false;
-      }
-  };
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (!head)
+            return false;
+        ListNode *tempA = head;
+        ListNode *tempB = head->next;
+        while (tempB && tempB->next)
+        {
+            if (tempA == tempB)
+                return true;
+            tempA = tempA->next;
+            tempB = tempB->next->next;
+        }
+        return false;
+    }
+};
